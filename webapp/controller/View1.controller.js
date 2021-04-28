60df1e6a597c77b5c05d4848c17b0f2d5ae2986d
Pooja Rokde <pooja.rokde@toro.com>
2021-04-28 12:10:12 +0000

    comitted

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -18,9 +18,25 @@ sap.ui.define([
                 // access OData model declared in manifest.json file
 
                 var oMetaModel = this.getOwnerComponent().getModel("searchHelp");
-                this.getView().setModel(oMetaModel,"help");
-
+                this.getView().setModel(oMetaModel, "help");
                 this._oView = this.getView();
+                var oPlant = new sap.ui.model.odata.v2.ODataModel("sap/opu/odata/sap/YMM_GMT_SRV", true, "", "");
+                oPlant.read("/gmt311Set", {
+                    success: function (oData, oResponse) {
+                        var data = oData.results[0];
+                        this._oView.byId("iPlant").setValue(data.Plant);
+                        this._oView.byId("iPlant").setEnabled(false);
+                    }.bind(this),
+                    error: function (oResponse) {
+                        // var errMessage = oResponse.headers["sap-message"];
+                        // var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+                        MessageBox.error(
+                            "Default Plant NOt Found.", {
+                            styleClass: bCompact ? "sapUiSizeCompact" : ""
+                        }
+                        );
+                    }.bind(this)
+                });
 
                 oViewM = this.getView();
                 // // set message model
@@ -39,6 +55,10 @@ sap.ui.define([
                 });
                 oModelM.setDefaultBindingMode(BindingMode.TwoWay);
                 oViewM.setModel(oModelM);
+
+                // this._oMultiInput = this.getView().byId("iMatnr");
+                // this.oColModel = new JSONModel(sap.ui.require.toUrl("ns/project1/model") + "/MatnrModel.json");
+                // this.oMaterialModel = new JSONModel(sap.ui.require.toUrl("sap/opu/odata/sap/YMM_GMT_SRV");
             },
             onMessagePopoverPress: function (oEvent) {
                 this._getMessagePopover().openBy(oEvent.getSource());
@@ -64,15 +84,6 @@ sap.ui.define([
                     MoveStloc: "STOP",
                     ITEM_TEXT: ""
                 };
-                // var oEntry = {};
-                // oEntry.Material = this.getView().byId("iMatnr").getValue();
-                // oEntry.Plant = this.getView().byId("iPlant").getValue();
-                // oEntry.StgeLoc = this.getView().byId("iFromSloc").getValue();
-                // oEntry.EntryQnt = this.getView().byId("iQuant").getValue();
-                // oEntry.EntryUom = this.getView().byId("iUom").getValue();
-                // oEntry.MovePlant = this.getView().byId("iPlant").getValue();
-                // oEntry.MoveStloc = this.getView().byId("iToSloc").getValue();               
-
 
                 oModel.create("/gmt311Set", oEntry, {
                     method: "POST",
@@ -140,6 +151,64 @@ sap.ui.define([
 
                     });
                 }
+            },
+            onMatnrHelpRequested: function () {
+                // var aCols = this.oColModel.getData().cols;
+
+                this._oValueHelpDialog = sap.ui.xmlfragment("ns.project1.view.MatnrSearch", this);
+                this.getView().addDependent(this._oValueHelpDialog);
+                	this._oValueHelpDialog.open();
+            },
+            //     this._oValueHelpDialog.getTableAsync().then(function (oTable) {
+            //         oTable.setModel(this.oProductsModel);
+            //         oTable.setModel(this.oColModel, "columns");
+
+            //         if (oTable.bindRows) {
+            //             oTable.bindAggregation("rows", "/ProductCollection");
+            //         }
+
+            //         if (oTable.bindItems) {
+            //             oTable.bindAggregation("items", "/ProductCollection", function () {
+            //                 return new ColumnListItem({
+            //                     cells: aCols.map(function (column) {
+            //                         return new Label({ text: "{" + column.template + "}" });
+            //                     })
+            //                 });
+            //             });
+            //         }
+            //         this._oValueHelpDialog.update();
+            //     }.bind(this));
+
+            //     this._oValueHelpDialog.setTokens(this._oMultiInput.getTokens());
+            //     this._oValueHelpDialog.open();
+            // },
+
+               onSlocHelpRequested: function () {
+                // var aCols = this.oColModel.getData().cols;
+
+                this._oValueHelpDialog = sap.ui.xmlfragment("ns.project1.view.slocSearch", this);
+                this.getView().addDependent(this._oValueHelpDialog);
+                	this._oValueHelpDialog.open();
+            },
+               onUOMHelpRequested: function () {
+                // var aCols = this.oColModel.getData().cols;
+
+                this._oValueHelpDialog = sap.ui.xmlfragment("ns.project1.view.UOMSearch", this);
+                this.getView().addDependent(this._oValueHelpDialog);
+                	this._oValueHelpDialog.open();
+            },
+            onValueHelpOkPress: function (oEvent) {
+                // var aTokens = oEvent.getParameter("tokens");
+                // this._oMultiInput.setTokens(aTokens);
+                this._oValueHelpDialog.close();
+            },
+
+            onValueHelpCancelPress: function () {
+                this._oValueHelpDialog.close();
+            },
+
+            onValueHelpAfterClose: function () {
+                this._oValueHelpDialog.destroy();
             }
         });
     });
